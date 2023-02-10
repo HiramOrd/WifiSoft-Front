578d5dabbf468ad8353c7e596e6773e37a4472e4
Hiram Ord <hiram8012@gmail.com>
2023-02-09 20:59:08 -0500

    feat (example)

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { PacksComponent } from './components/packs/packs.component';
 
-
+import { MenubarModule } from 'primeng/menubar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [HomeComponent, HeaderComponent, HeroComponent, PacksComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+
+    // NgPrime
+    BrowserAnimationsModule,
+    MenubarModule,
+    InputTextModule,
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
