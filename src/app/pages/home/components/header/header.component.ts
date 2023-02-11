f8f3c57a2e4965aa1951cadddf80b45af86e3a69
Noelí Rodríguez <noe.rmorales98@gmail.com>
2023-02-11 02:25:48 -0500

    quit primeng dependencies

--- src/app/pages/home/components/header/header.component.ts
+++ src/app/pages/home/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-header',
@@ -7,46 +6,9 @@ import {MenuItem} from 'primeng/api';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  items: MenuItem[] = [];
 
   constructor() {}
 
   ngOnInit(): void {
-    this.items = this.items = [
-      {
-        label: 'Inicio',
-        icon: 'pi pi-fw pi-home'
-      },
-      {
-        label: 'Paquetes',
-        icon: 'pi pi-fw pi-box',
-        items: [
-          {
-            label: 'Paquete 1',
-            icon: 'pi pi-fw pi-align-left',
-          },
-          {
-            label: 'Paquete 2',
-            icon: 'pi pi-fw pi-align-right',
-          },
-          {
-            label: 'Paquete 3',
-            icon: 'pi pi-fw pi-align-center',
-          },
-          {
-            label: 'Paquete 4',
-            icon: 'pi pi-fw pi-align-justify',
-          },
-        ],
-      },
-      {
-        label: 'Nosotros',
-        icon: 'pi pi-fw pi-users',
-      },
-      {
-        label: 'Contáctanos',
-        icon: 'pi pi-fw pi-id-card'
-      }
-    ];
   }
 }
