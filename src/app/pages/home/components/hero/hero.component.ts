e22c660124f3fbed60d4a15519c52e7bf5281393
Hiram Ord <hiram8012@gmail.com>
2023-02-22 13:18:55 -0500

    feat (hero) add logic example

--- src/app/pages/home/components/hero/hero.component.ts
+++ src/app/pages/home/components/hero/hero.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
+  @Input() title: String = '';
+  @Input() content: String = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
